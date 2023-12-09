b5187b5e589ceabc9392eda88a034e80835b3f7f
Tilman Frick <git@tilman.io>
2023-12-09 15:01:56 +0100

    feat: add basic header

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import { PlacesList } from '@/components/places-list'
 import styles from './page.module.scss'
 import { Wrapper } from '@/components/wrapper'
+import { Text, TextSize } from '@/components/text'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Wrapper>
+        <div className={styles.header}>
+          <Text size={TextSize.Large}>Our favorite places in Hamburg</Text>
+        </div>
         <PlacesList />
       </Wrapper>
     </main>
